40c555e26a3a6b55e54c8083690847c302d9be7e
Tucker Willenborg <admin@rivers.run>
2019-11-18 22:02:51 -0500

    Update races.py

--- races.py
+++ races.py
@@ -1,9 +1,10 @@
 #!/bin/python3
 
 from turtle import *
-from random import randint
+import random
 from time import sleep
 import colorsys
+import math
 
 print("Width: " + str(window_height()))
 print("Height: " + str(window_width()))
@@ -25,42 +26,52 @@ def upward(amount):
 	downward(-amount)
 	
 	
+def getTurtleY(turtleCount, position):
+	return window_height()/3 - (window_height()/1.55)/turtleCount*(position)
+
+def getLeftBound():
+	return -window_width() / 2.4
+
+def getTurtleX(ratio):
+	return getLeftBound() + (window_width()*3/4)*ratio
 	
 def drawTrack():
 	speed(0) #Max Speed.
 	
-	penup()
-	goto(-window_width() / 3,window_height()/3) #2/3rds of way to top left.
-	pendown()
-	
 	lines = 20		
+
+	hideturtle()
+	penup()
 	
 	for counter in range(lines + 1):
-		hideturtle()
+		goto(getTurtleX(counter/lines), getTurtleY(1, 0))
 		pendown()
 		write(int(counter * 100/lines), align="center")
-		downward(window_height()/2.9)
-		upward(window_height()/2.9)
+		downward(window_height()/1.5)
 		penup()
-		forward(window_width()/lines*2/3)
-
-	global end
-	end = pos()[0] #Position to end at.
+		
 
 	
 darkmode = True
 if darkmode:
 	bgcolor("black")
 	color("white")
+	
 
 def raceTurtles():
-	turtleCount = 12
+	turtleCount = 8
+	turtleSize = 5
+	
+	turtleCount = 24
+	turtleSize = 2
+	
 	turtles = []
 	for counter in range(turtleCount):
 		turtle = Turtle()
 		turtle.hideturtle()
 		turtle.shape("turtle")
-		turtle.shapesize(1.5,1.5) #Stretch to 1.5x in both dimensions
+		turtle.shapesize(turtleSize,turtleSize)
+		turtle.pensize(turtleSize)
 		
 		#Programmatically generate colors
 		hue = 360 / turtleCount * counter
@@ -76,10 +87,12 @@ def raceTurtles():
 		turtle = turtles[counter]
 		turtle.penup()
 		turtle.speed(0)
-		turtle.goto(-window_width()/3, window_height()/3 - (window_height()/3)/turtleCount*(counter+1))
+		turtle.goto(getLeftBound(), getTurtleY(len(turtles), counter + 1))
+		#TODO: Add numbers for each turtle.
+		turtle.pendown()
 		turtle.showturtle()
 		
-		
+	random.shuffle(turtles)
 	
 	sleep(0.5)
 	
@@ -92,6 +105,14 @@ def raceTurtles():
 	min = -window_width()/500	
 	max = window_width()/100
 	
+	if turtleCount > 10:
+		max = max*((turtleCount/10)**0.5) #Speed up the turtles slightly if there are lots. (Because the program runs slower due to large numbers of turtles)
+	
+	min = math.ceil(min)
+	max = math.ceil(max)
+	print(min, max)
+	
+	end = getTurtleX(1)
 	finished = False
 	
 	while finished == False:
@@ -100,20 +121,33 @@ def raceTurtles():
 			turtle = turtles[turtleNum]
 			
 			current = turtle.pos()[0]
-			print(current)
-			print(end)
 			
 			if current < end:
-				move = randint(min,max)
+				move = random.randint(min,max)
 				distances[turtleNum].append(move)
 				turtle.forward(move)
 				allfinished = False
 				
 		finished = allfinished
+	
+	topTurtles = []
+	leastMoves = 9999999999999999
+	for counter in range(len(turtles)):
+		moves = distances[counter]
+		if len(moves) < leastMoves:
+			topTurtles = [counter]
+			leastMoves = len(moves)
+		elif len(moves) == leastMoves:
+			topTurtles.append(counter)
 
-
-
-
+	#TODO: If two turtles tie on moves, check the total distance they went. If those are equal, have a tie.
+	for turtleNum in topTurtles:
+		turtle = turtles[turtleNum]
+		turtle.shapesize(turtleSize*2, turtleSize*2)
+		turtle.speed(1)
+		turtle.forward(window_width()/20)
+		turtle.right(1080)
+		
 
 		
 drawTrack()
